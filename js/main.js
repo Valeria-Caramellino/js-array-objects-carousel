99e409920918768e6f24505fea83e17e43affabb
valeria caramellino <valeriacaramellino@gmail.com>
2023-05-03 17:00:25 +0200

    tolto console log

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log("ciao");
+
 //creo elementi da mettere in pagina e dove
 let container = document.createElement("div");
 container.id = "container";
@@ -16,19 +16,23 @@ const images = [
         image: '01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
+    }, 
+    {
         image: '02.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
+    }, 
+    {
         image: '03.webp',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
+    }, 
+    {
         image: '04.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
+    }, 
+    {
         image: '05.webp',
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
@@ -37,8 +41,6 @@ const images = [
 
 //avvio un contatore quanto la lunghezza dell array
 for(let i=0; i<images.length; i++){
-    //prendo tutti i.valore che mi serve controllare
-    console.log(images[i].image, images[i].title, images[i].text);
     //inserisco html e contenuto nel box creando sotto elementi 
     box.innerHTML += `<div id="minibox"><h2>${images[i].title}</h2><p>${images[i].text}</p><img src="img/${images[i].image}" alt="img"></div>`;
     if ( i === 0 ){
